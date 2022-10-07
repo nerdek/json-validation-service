bf0a5c3d7ec2c5bd8cee15c96fce3d756baef75c
b.wikiera <nerdek91@gmail.com>
2022-10-07 21:08:35 +0200

    swagger route added

--- src/main/scala/com/wikiera/Main.scala
+++ src/main/scala/com/wikiera/Main.scala
@@ -1,7 +1,43 @@
 package com.wikiera
 
-import cats.effect.{IO, IOApp}
+import cats.effect._
+import cats.syntax.all._
+import com.wikiera.endpoints.Endpoints.swagger
+import com.wikiera.log.Logger
+import org.http4s.HttpRoutes
+import org.http4s.server.Router
+import org.http4s.blaze.server.BlazeServerBuilder
+import sttp.tapir._
+import sttp.tapir.server.http4s.Http4sServerInterpreter
 
-object Main extends IOApp.Simple {
-  override def run: IO[Unit] = IO(println("hello world"))
+import scala.concurrent.ExecutionContext
+import scala.util.control.NonFatal
+
+object HelloWorldHttp4sServer extends IOApp with Logger {
+  // the endpoint: single fixed path input ("hello"), single query parameter
+  // corresponds to: GET /hello?name=...
+  val helloWorld: PublicEndpoint[String, Unit, String, Any] =
+    endpoint.get.in("hello").in(query[String]("name")).out(stringBody)
+
+  // converting an endpoint to a route (providing server-side logic); extension method comes from imported packages
+  val helloWorldRoutes: HttpRoutes[IO] =
+    Http4sServerInterpreter[IO]().toRoutes(helloWorld.serverLogic(name => IO(s"Hello, $name!".asRight[Unit])))
+
+  val swaggerRoute = Http4sServerInterpreter[IO]().toRoutes(swagger)
+
+  implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    // starting the server
+    (for {
+
+      _ <- BlazeServerBuilder[IO]
+        .withExecutionContext(ec)
+        .bindHttp(8080, "localhost")
+        .withHttpApp(Router("/" -> helloWorldRoutes, "/" -> swaggerRoute).orNotFound)
+        .serve
+    } yield {}).compile.drain.as(ExitCode.Success).handleErrorWith { case NonFatal(e) =>
+      IO(logger.error("Error in json-validator service", e)).as(ExitCode.Error)
+    }
+  }
 }
